7e8567e7833e7e053a6e7b6bbbd1e08ea54fc4c6
Josef Hardi <josef.hardi@gmail.com>
2022-12-21 16:46:28 -0600

    Implement a validator to check the presence of required values

---
+++ src/main/java/org/metadatacenter/spreadsheetvalidator/algorithm/PropNames.java
@@ -0,0 +1,14 @@
+package org.metadatacenter.spreadsheetvalidator.algorithm;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class PropNames {
+
+  public static final String ERROR_TYPE = "errorType";
+
+  public static final String SUGGESTION = "suggestion";
+  public static final String POSSIBLE_OPTIONS = "possibleOptions";
+  public static final String SEVERITY = "severity";
+}
--- src/main/java/org/metadatacenter/spreadsheetvalidator/algorithm/RequiredFieldValidator.java
+++ src/main/java/org/metadatacenter/spreadsheetvalidator/algorithm/RequiredFieldValidator.java
@@ -1,13 +1,18 @@
 package org.metadatacenter.spreadsheetvalidator.algorithm;
 
 import org.metadatacenter.spreadsheetvalidator.AbstractValidator;
+import org.metadatacenter.spreadsheetvalidator.RepairClosures;
 import org.metadatacenter.spreadsheetvalidator.ValidationError;
+import org.metadatacenter.spreadsheetvalidator.ValidationResult;
 import org.metadatacenter.spreadsheetvalidator.ValidatorContext;
+import org.metadatacenter.spreadsheetvalidator.domain.ColumnDescription;
 import org.metadatacenter.spreadsheetvalidator.domain.SpreadsheetRow;
 import org.metadatacenter.spreadsheetvalidator.util.ValueAssertion;
 
 import javax.annotation.Nonnull;
 
+import static org.metadatacenter.spreadsheetvalidator.algorithm.PropNames.ERROR_TYPE;
+import static org.metadatacenter.spreadsheetvalidator.algorithm.PropNames.SEVERITY;
 import static org.metadatacenter.spreadsheetvalidator.util.Matchers.isNullOrEmpty;
 
 /**
@@ -15,29 +20,28 @@ import static org.metadatacenter.spreadsheetvalidator.util.Matchers.isNullOrEmpt
  * Stanford Center for Biomedical Informatics Research
  */
 public class RequiredFieldValidator extends AbstractValidator {
+
   @Override
-  public boolean validate(@Nonnull ValidatorContext context, @Nonnull SpreadsheetRow spreadsheetRow) {
-    var spreadsheetDefinition = context.getSpreadsheetDefinition();
-    return spreadsheetRow.columnStream()
-        .map(columnName -> {
-          var columnDescription = spreadsheetDefinition.getColumnDescription(columnName);
-          if (columnDescription.isRequiredColumn()) {
-            var value = spreadsheetRow.getValue(columnName);
-            if (ValueAssertion.equals(value, isNullOrEmpty())) {
-              context.getValidationResult().add(
-                  ValidationError.builder()
-                      .setRowNumber(spreadsheetRow.getRowNumber())
-                      .setColumnName(columnName)
-                      .setInvalidValue(value)
-                      .setErrorDescription("Required value is missing")
-                      .setOtherProp("severeLevel", 5)
-                      .setOtherProp("errorType", "missingRequired")
-                      .build());
-              return false;
-            }
-          }
-          return true;
-        })
-        .allMatch(x -> x);
+  public boolean validateOnEach(@Nonnull Integer rowNumber,
+                                @Nonnull String columnName,
+                                @Nonnull Object value,
+                                @Nonnull ColumnDescription columnDescription,
+                                @Nonnull RepairClosures repairClosures,
+                                @Nonnull ValidationResult validationResult) {
+    if (columnDescription.isRequiredColumn()) {
+      if (ValueAssertion.equals(value, isNullOrEmpty())) {
+        validationResult.add(
+            ValidationError.builder()
+                .setRowNumber(rowNumber)
+                .setColumnName(columnName)
+                .setInvalidValue(value)
+                .setErrorDescription("Required value is missing")
+                .setOtherProp(ERROR_TYPE, "missingRequired")
+                .setOtherProp(SEVERITY, 5)
+                .build());
+        return false;
+      }
+    }
+    return true;
   }
 }
