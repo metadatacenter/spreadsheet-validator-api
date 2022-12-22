40ed254e40da717193318e4a15860a86c98e2238
Josef Hardi <josef.hardi@gmail.com>
2022-12-22 15:31:15 -0600

    Implement a number extractor from a string

---
+++ src/main/java/org/metadatacenter/spreadsheetvalidator/algorithm/NumberExtractor.java
@@ -0,0 +1,32 @@
+package org.metadatacenter.spreadsheetvalidator.algorithm;
+
+import org.metadatacenter.spreadsheetvalidator.Closure;
+
+import javax.annotation.Nullable;
+import java.util.regex.Pattern;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class NumberExtractor implements Closure<Number> {
+
+  private final String numberOnlyPattern = "(\\d+(?:\\.\\d+)?)";
+  private final Pattern  pattern = Pattern.compile(numberOnlyPattern, Pattern.MULTILINE);
+
+  @Override
+  @Nullable
+  public Number execute(Object... inputs) {
+    var string = String.valueOf(inputs[0]);
+    var matcher = pattern.matcher(string);
+    if (matcher.find()) {
+      var firstMatch = matcher.group(1);
+      if (firstMatch.contains(".")) {
+        return Double.valueOf(firstMatch);
+      } else {
+        return Integer.valueOf(firstMatch);
+      }
+    }
+    return null;
+  }
+}
