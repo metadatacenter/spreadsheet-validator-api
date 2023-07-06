607b765f01b36584623d1f4d03814eaf922f631f
Josef Hardi <josef.hardi@gmail.com>
2023-07-05 20:35:22 -0500

    Added terminology endpoint config

--- src/main/java/org/metadatacenter/spreadsheetvalidator/CedarConfig.java
+++ src/main/java/org/metadatacenter/spreadsheetvalidator/CedarConfig.java
@@ -15,12 +15,14 @@ public abstract class CedarConfig {
 
   private static final String API_KEY = "apiKey";
   private static final String BASE_URL = "baseUrl";
+  private static final String TERMINOLOGY_ENDPOINT = "terminologyEndpoint";
 
   @Nonnull
   @JsonCreator
   public static CedarConfig create(@Nonnull @JsonProperty(API_KEY) String apiKey,
-                                   @Nonnull @JsonProperty(BASE_URL) String baseUrl) {
-    return new AutoValue_CedarConfig(apiKey, baseUrl);
+                                   @Nonnull @JsonProperty(BASE_URL) String baseUrl,
+                                   @Nonnull @JsonProperty(TERMINOLOGY_ENDPOINT) String terminologyEndpoint) {
+    return new AutoValue_CedarConfig(apiKey, baseUrl, terminologyEndpoint);
   }
 
   @Nonnull
@@ -30,4 +32,8 @@ public abstract class CedarConfig {
   @Nonnull
   @JsonProperty(BASE_URL)
   public abstract String getBaseUrl();
+
+  @Nonnull
+  @JsonProperty(TERMINOLOGY_ENDPOINT)
+  public abstract String getTerminologyEndpoint();
 }
