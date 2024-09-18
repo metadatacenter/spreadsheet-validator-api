a20a1944b179cbb0530c62ff9310048b0812587f
Josef Hardi <josef.hardi@gmail.com>
2024-09-18 11:50:55 -0700

    Fix cast error

--- src/main/java/org/metadatacenter/spreadsheetvalidator/validator/DefaultValidationReportHandler.java
+++ src/main/java/org/metadatacenter/spreadsheetvalidator/validator/DefaultValidationReportHandler.java
@@ -8,13 +8,7 @@ import org.metadatacenter.spreadsheetvalidator.ValidationReportItem;
 
 import java.util.List;
 
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.COLUMN_LABEL;
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.COLUMN_NAME;
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.ERROR_MESSAGE;
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.ERROR_TYPE;
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.ROW_INDEX;
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.SUGGESTION;
-import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.VALUE;
+import static org.metadatacenter.spreadsheetvalidator.validator.PropNames.*;
 
 /**
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
@@ -28,7 +22,7 @@ public class DefaultValidationReportHandler implements ValidationReportHandler {
         .map(validationError ->
             ValidationReportItem.create(
                 validationError.getErrorLocation().getRowIndex() + 1,
-                (String) validationError.getOtherProp(COLUMN_LABEL),
+                String.valueOf(validationError.getOtherProp(COLUMN_LABEL)),
                 validationError.getErrorLocation().getColumnName(),
                 validationError.getOtherProp(VALUE),
                 validationError.getErrorType(),
